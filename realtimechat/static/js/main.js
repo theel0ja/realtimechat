ce9660b1178f972fdda1c2f7ce8e5b686bc408e9
Elias Ojala <git@eliasojala.me>
2018-03-02 23:28:31 +0200

    chat_page.html - Form handler

--- realtimechat/static/js/main.js
+++ realtimechat/static/js/main.js
@@ -1,8 +1,15 @@
 console.log("DEBUG: js/script.js loaded");
 
-/* chat_page.html */
+/* chat_page.html - Form handler */
 $( "form#chatForm" ).submit(function(e) {
-  alert("Chat form submitted!");
+  var messageBox = $( "input#messageBox" );
+
+  var message = messageBox.val();
+
+  // TODO: If textbox is empty, show error message
+
+  // Empty messageBox
+  messageBox.val(''); // FIXME: Fix this problem with the required thing on the input (Video: https://storage.theel0ja.info/theel0ja-img-up/ShareX/2018/03/2018-03-02_23-26-18.mp4)
 
   e.preventDefault();
 });
