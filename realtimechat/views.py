f8d0974d7693975a3a6e659a69c3f88c6bb50d15
Elias Ojala <git@eliasojala.me>
2018-03-02 19:48:14 +0200

    Created a chat page.

--- realtimechat/views.py
+++ realtimechat/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/chat')
+def chat_page():
+    return render_template('chat_page.html')
